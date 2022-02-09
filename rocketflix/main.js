65a895b5a5e1723e33216723c6a72bda4b9daa66
edimilsonbraz <edimilson.gt8@gmail.com>
2022-02-09 18:18:49 -0300

    :alien: Consuming External API changes

--- rocketflix/main.js
+++ rocketflix/main.js
@@ -3,3 +3,13 @@ import {
   IMG_URL,
   language,
 } from './api.js'
+
+const url = `${BASE_URL}550?api_key=${API_KEY}`
+
+fetch(url)
+.then((response) => response.json())
+.then(data => {
+  contentImg.textContent = data.poster_path
+  titleMovie.textContent = data.original_title
+  textMovie.textContent = data.overview
+})

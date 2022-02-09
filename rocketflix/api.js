23308e150d623f1afa87519cdf422cf6fcb6671e
edimilsonbraz <edimilson.gt8@gmail.com>
2022-02-09 17:25:32 -0300

    :tada: Begin a project Rocketflix

---
+++ rocketflix/api.js
@@ -0,0 +1,6 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+
+export const API_KEY = 'f4c42d7f1d3d45b8375f9d29e4c9afbc';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';

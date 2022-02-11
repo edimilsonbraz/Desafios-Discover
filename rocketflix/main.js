2674a6cc5deab4c25804f13feea6c09e666c4dad
edimilsonbraz <edimilson.gt8@gmail.com>
2022-02-11 20:36:55 -0300

    :fire: fixing error in external API

--- rocketflix/main.js
+++ rocketflix/main.js
@@ -4,19 +4,27 @@ import {
   language,
 } from './api.js'
 
-const url = `${BASE_URL}550?api_key=${API_KEY}&${language}`
-const urlImage = `${IMG_URL}`
+// const url = `${BASE_URL}550?api_key=${API_KEY}&${language}`
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=f4c42d7f1d3d45b8375f9d29e4c9afbc&query=action'
 const img = document.querySelector('.content img')
+const button = document.querySelector('.btn')
+const urlImage = `${IMG_URL}`
+
+button.addEventListener('click', getMoviesApi)
+
+const response = await fetch(url)
+const data = await response.json()
+const newData = data.results
 
 function getMoviesApi() {
-  fetch(url)
-  .then((response) => response.json())
-  .then(data => {
-    titleMovie.textContent = data.original_title
-    textMovie.textContent = data.overview
-    
-    img.setAttribute('src',`${urlImage}` + data.poster_path)
+  newData.push(newData.splice(0,1)[0]); //muda a posição do index do array
+  newData.forEach(result => {
+    titleMovie.textContent = result.title
+    textMovie.textContent = result.overview
+    img.setAttribute('src',`${urlImage}` + result.poster_path)
   })
 }
 
-getMoviesApi()
+
+
+

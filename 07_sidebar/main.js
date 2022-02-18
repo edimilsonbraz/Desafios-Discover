c413883d1545773563df5f7fc9d951a156f353e1
edimilsonbraz <edimilson.gt8@gmail.com>
2022-02-18 09:23:42 -0300

    :sparkles: Add feature(dark mode)

--- 07_sidebar/main.js
+++ 07_sidebar/main.js
@@ -1,6 +1,13 @@
 const menu = document.querySelector('.menu')
 const sidebar = document.querySelector('.sidebar')
+const body = document.querySelector('body')
+const modeSwtich = document.querySelector('.toggle-switch')
+console.log(modeSwtich)
 
 menu.onclick = () => {
   sidebar.classList.toggle('toggle')
 }
+
+modeSwtich.addEventListener('click', () => {
+  body.classList.toggle("dark")
+})

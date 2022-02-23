d06592f142357ad0d4893237b40774b894965d46
edimilsonbraz <edimilson.gt8@gmail.com>
2022-02-23 09:15:33 -0300

    :sparkles: Add Feature LocalStorage Preferences themes

--- 02_theme-switcher/main.js
+++ 02_theme-switcher/main.js
@@ -1,8 +1,23 @@
-const theme = document.querySelector('.light')
+const body = document.querySelector('body')
 const Input = document.getElementById('theme')
 
 Input.addEventListener('click', toggleTheme)
 
 function toggleTheme() {
-  theme.classList.toggle('dark')
+  // body.classList.toggle('dark')
+  if(body.classList.contains('dark')) {
+    body.classList.remove('dark')
+    body.classList.add('light')
+    localStorage.setItem('theme', 'light')
+  }
+  else{
+    body.classList.remove('light')
+    body.classList.add('dark')
+    localStorage.setItem('theme', 'dark')
+  }
+  document.getElementById('currentTheme').innerHTML = localStorage.theme;
 }
+
+body.classList.add(localStorage.getItem('theme'))
+document.getElementById('currentTheme').innerHTML = localStorage.theme;
+
